db28f6465bc54689dfc98cb021061ff6b47321c0
athallahyoga11 <112833954+athallahyoga11@users.noreply.github.com>
2022-12-02 19:51:22 +0700

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router";
 import LoginView from "../components/login.vue";
 import RegisterView from "../components/register.vue";
+import DashboardView from "../components/dashboard.vue";
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -17,6 +19,11 @@ const router = createRouter({
       component: RegisterView,
     },
 
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: DashboardView,
+    },
   
   ],
 });
